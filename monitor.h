6137fd6047783c59670c2a6c812eb9a64110edf0
bhumikaadobe <bhumi.umar@gmail.com>
2025-05-29 17:03:45 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -8,6 +8,8 @@ bool isSpo2Ok(float spo2);  // check spo2 normal range
 
 int isVitalsOk(float temperature, float pulseRate, float spo2);  // check vitals normal range
 
+void Alert(VitalStatus status);
+
 enum class VitalStatus {
     OK,
     TempOutOfRange,
