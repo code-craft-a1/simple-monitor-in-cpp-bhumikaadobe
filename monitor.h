6efd2c7fe75d00b6fb78b4000076ee8ec834b328
bhumikaadobe <bhumi.umar@gmail.com>
2025-05-29 16:20:23 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,9 +1,9 @@
 #pragma once
 
-bool isTemperatureOk(float temperature); //check temperatue normal range
+bool isTemperatureOk(float temperature);  //check temperatue normal range
 
-bool PulseRateOk(float pulseRate); //check pulse normal range
+bool PulseRateOk(float pulseRate);  //check pulse normal range
 
 bool isSpo2Ok(float spo2);  //check spo2 normal range
 
-int vitalsOk(float temperature, float pulseRate, float spo2); //check vitals normal range
+int vitalsOk(float temperature, float pulseRate, float spo2);  //check vitals normal range
