d3c6447c946b60426f62b67572cabc6b9728cac1
bhumikaadobe <bhumi.umar@gmail.com>
2025-05-29 16:24:03 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -5,25 +5,20 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-bool isTemperatureOk(float temperature)
-{
+bool isTemperatureOk(float temperature) {
   return temperature >= 95 && temperature <= 102;
 }
 
-bool isPulseRateOk(float pulseRate)
-{
+bool isPulseRateOk(float pulseRate) {
   return pulseRate >= 60 && pulseRate <= 100;
 }
 
-bool isSpo2Ok(float spo2)
-{
+bool isSpo2Ok(float spo2) {
   return spo2 >= 90;
 }
 
-void Alert()
-{
-    for (int i = 0; i < 6; i++)
-    {
+void Alert(){
+    for (int i = 0; i < 6; i++) {
         cout << "\r* " << flush;
         sleep_for(seconds(1));
         cout << "\r *" << flush;
@@ -31,22 +26,18 @@ void Alert()
     }
 }
 
-int vitalsOk(float temperature, float pulseRate, float spo2)
-{
-  if (!isTemperatureOk(temperature))
-  {
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  if (!isTemperatureOk(temperature) {
     cout << "Temperature is critical!\n";
     Alert();
     return 0;
   }
-  if (!isPulseRateOk(pulseRate))
-  {
+  if (!isPulseRateOk(pulseRate)) {
     cout << "Pulse Rate is out of range!\n";
     Alert();
     return 0;
   }
-  if (!isSpo2Ok(spo2))
-  {
+  if (!isSpo2Ok(spo2)) {
     cout << "Oxygen Saturation out of range!\n";
     Alert();
     return 0;
